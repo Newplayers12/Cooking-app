ee4a52af6392ee1f3aa3eeffc1b254de9f50d6ba
Tran Khanh <ptrankhanh02@gmail.com>
2022-12-23 23:28:35 +0700

    Add change user avatar

--- Cooking_app/base/views.py
+++ Cooking_app/base/views.py
@@ -82,14 +82,13 @@ def signup_acc(request):
     context = {}
     template_name = 'signup.html'
     if request.method == 'POST':
-        fullname = request.POST.get('fullname-signup-input')
-        email = request.POST.get('email-signup-input')
-        username = request.POST.get('username-signup-input')
-        password1 = request.POST.get('password-signup-input')
-        password2 = request.POST.get('repassword-signup-input')
+        fullname = request.POST.get('fullname-signup-input').strip()
+        email = request.POST.get('email-signup-input').strip()
+        username = request.POST.get('username-signup-input').strip()
+        password1 = request.POST.get('password-signup-input').strip()
+        password2 = request.POST.get('repassword-signup-input').strip()
         gender = request.POST.get('gender')
         
-        print(request.POST['gender'])
         context = {
             'fullname': fullname,
             'email': email,
@@ -107,6 +106,7 @@ def signup_acc(request):
             if dup_email:
                 messages.error(request, "Email was already signed up.")
             return render(request, template_name, context)
+            
         user = User.objects.create(
             username=username,
             password=make_password(password1),
@@ -126,8 +126,8 @@ def login_acc(request):
     context = {}
     template_name = 'login.html'
     if request.method == 'POST':
-        username = request.POST.get('username-input')
-        password = request.POST.get('password-input')
+        username = request.POST.get('username-input').strip()
+        password = request.POST.get('password-input').strip()
         context = {
             'username': username,
         }
@@ -147,12 +147,7 @@ def logout_acc(request):
     
 @login_required(login_url='login')
 def profile_acc(request):
-    """TODO
-        show user default avatar onto profile: {{ obj.image.url }},
-        cannot add default value in input tag type file?
-        or just add more tags, ex: <img></img>
-        src: https://stackoverflow.com/a/70485949
-        strip(): fullname, password, username ... ?
+    """ TODO: Saved recipes, My Posts
     """
     context = {}
     template_name = 'profile.html'
@@ -160,29 +155,32 @@ def profile_acc(request):
     if request.method == 'POST':
         new_fullname = request.POST.get('fullname-input')
         new_password = request.POST.get('newpassword')
+        new_avatar = request.FILES.get('img-profile')
         new_gender = request.POST.get('gender')
         new_phone = request.POST.get('phone')
         new_bday = request.POST.get('birthday')
 
         # Case: Compare old and new password
         if new_password:
+            new_password = new_password.strip()
             if user_info.user.check_password(new_password):
                 messages.error(request, "New password cannot be the same as your old password.")
             else:
                 user_info.user.set_password(new_password)
                 user_info.user.save()
         if new_fullname:
-            user_info.fullname = new_fullname
+            user_info.fullname = new_fullname.strip()
+        if new_avatar:
+            user_info.avatar = new_avatar
         if new_gender:
             user_info.gender = new_gender
         if new_phone:
-            user_info.phone = new_phone
+            user_info.phone = new_phone.strip()
         if new_bday:
             user_info.bday = new_bday
-        if request.FILES['img-profile']:
-            user_info.avatar = request.FILES['img-profile']
+        
+        user_info.save(update_fields=['fullname', 'avatar', 'gender', 'phone', 'bday'])
 
-        user_info.save(update_fields=['fullname', 'gender', 'phone', 'bday', 'avatar'])
     context = {
         'user_info': user_info,
     }
@@ -194,99 +192,3 @@ def PostARecipe(request):
     context = {}
     template_name = "post_a_recipe.html"
     return render(request, template_name, context)
-
-
-
-
-
-
-
-
-
-        
-
-        
-
-
-
-
-"""
-We will start again from here on now
-"""
-
-# class Authentication(TemplateView):
-#     f_login = LoginForm
-#     f_signup = SignupForm
-#     context = {}
-#     template_name = 'login_signup.html'
-
-#     def get(self, request, *args, **kwargs):
-#         self.context = {
-#             'login_form': self.f_login(),
-#             'signup_form': list(self.f_signup(request=request)),
-#         }
-#         return render(request, self.template_name, self.context)
-    
-#     def post(self, request, *args, **kwargs):
-#         # Case: Login information is submitted
-#         if 'login' in request.POST:
-#             form = self.f_login(request.POST)
-#             if form.is_valid():
-#                 username = form.cleaned_data.get('username')
-#                 password = form.cleaned_data.get('password')
-#                 user = authenticate(request, username=username, password=password)
-#                 if user:
-#                     login(request, user)
-#                     return redirect('/')
-#                 else:
-#                     messages.error(request, "Email or Password is incorrect.")
-#             self.context = {
-#                 'login_form': form,
-#                 'signup_form': list(self.f_signup(request=request)),
-#             }
-#         # Case: Signup information is submitted
-#         if 'signup' in request.POST:
-#             form = self.f_signup(request.POST, request=request)
-#             if form.is_valid():
-#                 user = form.save()
-#                 login(request, user)
-#                 return redirect('/')
-#             self.context = {
-#                 'login_form': self.f_login(),
-#                 'signup_form': list(form),
-#             }
-#         return render(request, self.template_name, self.context)
-
-
-# @method_decorator(login_required(login_url='accounts/login'), name='dispatch')
-# class PersonalInfo(TemplateView):
-#     form_class = ProfileForm
-#     context = {}
-#     template_name = 'profile.html'
-
-#     def get_object(self, request):
-#         return UserInfo.objects.get(user=request.user)
-
-#     def get(self, request, *args, **kwargs):
-#         user_info = self.get_object(request)
-#         self.context = {
-#             'user_info': self.form_class(request.POST, instance=user_info),
-#             # 'user_recipes': UserRecipe.objects.filter(created_by=user_info.id),
-#             #TODO: UserRecipe Model
-#         }
-#         return render(request, self.template_name, self.context)
-
-#     def post(self, request, *args, **kwargs):
-#         user_info = self.get_object(request)
-#         form = self.form_class(request.POST, instance=user_info)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('profile')
-
-#         print(form)
-#         """ self.context = {
-#             'user_info': form,
-#             # 'user_recipes': UserRecipe.objects.filter(created_by=user_info.id),
-#             #TODO: UserRecipe Model
-#         } """
-#         return render(request, self.template_name, self.context)
